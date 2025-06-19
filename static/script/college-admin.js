420af26707b48b91fe1411e342f35927334d9bfe
Sahid-Rizwan <sahidrizwan.m@innovationminds.com>
2025-06-19 16:00:24 +0530

    Completed the form validation

--- static/script/college-admin.js
+++ static/script/college-admin.js
@@ -30,7 +30,7 @@ const otherclgEvents = [{
     }
 ]
 
-const otherclgContainer = document.querySelector("#otherclg-event");
+const otherclgContainer = document.getElementById("otherclg-event");
 
 otherclgEvents.forEach(event => {
     const eventHTML = `
@@ -69,16 +69,142 @@ otherclgEvents.forEach(eventDes => {
     `;
     Description.innerHTML += eventDesc;
 })
-
 //clg code input
-document.getElementById("reg-form").addEventListener("submit", function(event) {
-    const collegeCode = document.getElementById("clg-code");
-    const alertmsg = document.getElementById("clg-code-req");
+// document.getElementById("submit-btn").addEventListener("submit", function(event) {
+//     const collegeCode = document.getElementById("clg-code");
+//     const alertmsg = document.getElementById("clg-code-req");
 
-    if (collegeCode.value.trim() === "") {
-        event.preventDefault();
-        alertmsg.hidden = false;
+//     if (collegeCode.value.trim() === "") {
+//         event.preventDefault();
+//         alertmsg.classList.remove("hidden");
+//     } else {
+//         alertmsg.classList.add("hidden");
+//     }
+// });
+const collegeCode = document.getElementById("clg-code");
+const collegecodeAlertmsg = document.getElementById("clg-code-req");
+function validatecollegeCode(){
+        const code = collegeCode.value.trim();
+        if(code==""){
+            collegecodeAlertmsg.textContent = "Enter the College Code";
+            return false;
+        }
+        else if(isNaN(code)){
+            collegecodeAlertmsg.textContent = "Kindly, use Numbers";
+            return false;
+        }
+        else if(code.length!=4){
+            collegecodeAlertmsg.textContent = "only 4 digit is allowed";
+            return false;
+        }
+        
+        else{
+            collegecodeAlertmsg.textContent = "";
+            return true;
+        }
+}
+
+const collegeName = document.getElementById("clg-name");
+const collegenameAlertmsg = document.getElementById("clg-name-req");
+function validatecollegeName(){
+    const clgName = collegeName.value.trim();
+    if(clgName==""){
+        collegenameAlertmsg.textContent = "Enter the College Name";
+    }
+    else if(clgName.length<3){
+        collegenameAlertmsg.textContent = "Minimum 3 characters Needed";
+        return false;
+    }
+    else if(clgName.length>60){
+        collegenameAlertmsg.textContent = "Maximum 60 characters are allowed";
+        return false;
+    }
+    else{
+        collegenameAlertmsg.textContent = "";
+        return true;
+    }
+}
+
+const fullnameInput = document.getElementById("name");
+const fullnameAlertmsg = document.getElementById("full-name-req");
+function validatefullName(){
+    const fullName = fullnameInput.value.trim();
+    if(fullName==""){
+        fullnameAlertmsg.textContent = "Enter the full name";
+        return false;
+    }else if(fullName.length<3){
+        fullnameAlertmsg.textContent = "Enter a valid name, minimum 3 characters needed";
+        return false;
+    }
+    else{
+        fullnameAlertmsg.textContent = "";
+        return true;
+    }
+}
+
+const emailInput = document.getElementById("email");
+const emailAlertmsg = document.getElementById("email-req");
+const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+function validateEmail(){
+    const email = emailInput.value.trim();
+    if(email==""){
+        emailAlertmsg.textContent = "Email is required";
+        return false;
+    }else if(!emailFormat.test(email)){
+        emailAlertmsg.textContent = "Enter a valid email";
+        return false;
+    }else{
+        emailAlertmsg.textContent = "";
+        return true;
+    }
+}
+
+const departmentName = document.getElementById("dept");
+const deptnameAlertmsg = document.getElementById("dept-req");
+function validateDepartment(){
+    const deptName = departmentName.value.trim();
+    if(deptName==""){
+        deptnameAlertmsg.textContent = "Department is required";
+    }
+    else if(deptName.length<2){
+        deptnameAlertmsg.textContent = "Minimum 2 characters Needed";
+        return false;
+    }
+    else if(deptName.length>30){
+        deptnameAlertmsg.textContent = "Maximum 30 characters are allowed";
+        return false;
+    }
+    else{
+        deptnameAlertmsg.textContent = "";
+        return true;
+    }
+}
+
+collegeCode.addEventListener("input",validatecollegeCode)
+collegeName.addEventListener("input",validatecollegeName)
+fullnameInput.addEventListener("input",validatefullName)
+emailInput.addEventListener("input",validateEmail)
+departmentName.addEventListener("input",validateDepartment)
+
+const message = document.getElementById("submitAlertmsg");
+const form = document.getElementById("reg-form");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const isValid =
+      validatecollegeCode() &&
+      validatecollegeName() &&
+      validatefullName() &&
+      validateEmail() &&
+      validateDepartment();
+    if (isValid) {
+      message.textContent = "Registered successfully!";
+      message.style.color = "green";
+      form.reset();
+     
+    //   [collegecodeAlertmsg, collegenameAlertmsg, fullnameAlertmsg, emailAlertmsg, deptnameAlertmsg].forEach( el => el.textContent = "");
     } else {
-        alertmsg.hidden = true;
+      message.textContent = "Please enter Valid details.";
+      message.style.color = "red";
     }
 });
