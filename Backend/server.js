d2ef2111a7b2801fdcd2116c750a9d6a60418652
Sahid-Rizwan <sahidrizwan.m@innovationminds.com>
2025-07-03 19:29:14 +0530

    The full Nav bar is taken dynamically from backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,128 +1,37 @@
-// //creating a new server using node.js
-
-// // console.log("hello, world!");
-// const backendData = require('./dataModels/backendData.js');
-// const frontendData = require('./dataModels/frontendData.js');
-
-// // //http module will be used
-// // const http = require('http'); //importing http package
-// // const protocol = "http://";
-// // let host = "localhost";
-// // let baseUrl = "";
-// // const server = http.createServer((req,res) => {
-// //     host = req.headers.host;
-// //     res.writeHead(200,{
-// //         "Conten-Type":backendData.contentType.TEXTPLAIN,
-// //     });
-// //     res.end("Hello, World!");
-// // });
-
-// // const port = 3000;
-// // server.listen(port, () => {
-// //     console.log(`sever is running at ${protocol}${host}:${port}`)
-// // });
-
-// const http = require("http");
-// const fs = require("fs");
-// const path = require("path");
-
-// const server = http.createServer((req, res) => {
-//     if (req.url === "/") {
-//         const filePath = path.join(__dirname, "public", "/Frontend/templates/home.html");
-//         fs.readFile(filePath, (err, content) => {
-//             if (err) {
-//                 res.writeHead(200, { "Content-Type": backendData.contentType.TEXTHTML });
-//                 res.end(content);
-                
-//             } else {
-//                 res.writeHead(500);
-//                 res.end("Error loading page");
-//             }
-//         });
-//     } else if (req.url === "/api/navbar") {
-//         res.writeHead(200, { "Content-Type": backendData.contentType.APPJSON});
-//         res.end(JSON.stringify(frontendData.navData));
-//     }
-
-//     else {
-//         const filePath = path.join(__dirname, "public", req.url);
-//         fs.readFile(filePath, (err, content) => {
-//             if (err) {
-//                 res.writeHead(404);
-//                 res.end("Not Found");
-//             } else {
-//                 // Guess content type based on file extension
-//                 let ext = path.extname(req.url);
-//                 let contentType = "text/plain";
-//                 if (ext === ".css") contentType = "text/css";
-//                 else if (ext === ".js") contentType = "application/javascript";
-//                 else if (ext === ".jpg" || ext === ".jpeg") contentType = "image/jpeg";
-//                 else if (ext === ".png") contentType = "image/png";
-//                 else if (ext === ".html") contentType = "text/html";
-
-//                 res.writeHead(200, { "Content-Type": contentType });
-//                 res.end(content);
-//             }
-//         });
-//     }
-// });
-
-// server.listen(5500, () => {
-//     console.log("Server running on http://localhost:");
-// });
+//creating a new server using node.js
 
 const backendData = require('./dataModels/backendData.js');
 const frontendData = require('./dataModels/frontendData.js');
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
 
-const server = http.createServer((req, res) => {
-    // Serve HTML page
-    if (req.url === "/") {
-        const filePath = path.join(__dirname,"Frontend", "templates", "home.html");
-        fs.readFile(filePath, (err, content) => {
-            if (err) {
-                console.error("READ ERROR:", filePath, err);
-                res.writeHead(500);
-                return res.end("Error loading page");
-            } else {
-                res.writeHead(200, { "Content-Type": "text/html" }); // ✅ FIXED here
-                res.end(content);
-            }
+//http module will be used
+const http = require('http'); //importing http package
+const protocol = "http://";
+let host = "localhost";
+let baseUrl = "";
+const server = http.createServer((req,res) => {
+    host = req.headers.host;
+    // if (req.url === '/' && req.method === 'GET') {
+        const combinedData = {
+            navTitle: frontendData.navTitle,
+            navData: frontendData.navData
+        };
+        res.writeHead(200, { 
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*' 
         });
-    }
+        res.end(JSON.stringify(combinedData));
+// }
+    // res.writeHead(200,{
+    //     "Content-Type":backendData.contentType.APPJSON,
+    //     "access-control-allow-origin":"*"
+    // });
+    // res.end(JSON.stringify(frontendData.navTitle));
+    // res.end(JSON.stringify(frontendData.navData));
     
-    // Serve navbar API
-    else if (req.url === "/api/navbar") {
-        res.writeHead(200, { "Content-Type": backendData.contentType.APPJSON });
-        res.end(JSON.stringify(frontendData.navData));
-    }
-
-    // Serve static files (CSS, JS, Images)
-    else {
-        const filePath = path.join(__dirname, "public", req.url);
-        fs.readFile(filePath, (err, content) => {
-            if (err) {
-                res.writeHead(404);
-                res.end("Not Found");
-            } else {
-                // Guess content type based on file extension
-                let ext = path.extname(req.url);
-                let contentType = "text/plain";
-                if (ext === ".css") contentType = "text/css";
-                else if (ext === ".js") contentType = "application/javascript";
-                else if (ext === ".jpg" || ext === ".jpeg") contentType = "image/jpeg";
-                else if (ext === ".png") contentType = "image/png";
-                else if (ext === ".html") contentType = "text/html";
-
-                res.writeHead(200, { "Content-Type": contentType });
-                res.end(content);
-            }
-        });
-    }
 });
 
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const port = 3000;
+server.listen(port, () => {
+    console.log(`sever is running at ${protocol}${host}:${port}`)
 });
+
--- Frontend/script/home.js
+++ Frontend/script/home.js
@@ -1,11 +1,55 @@
-fetch("/api/navbar")
-    .then(res => res.json())
-    .then(data => {
-        const menu = document.getElementById("sidemenu");
-        menu.innerHTML = "";
-        data.forEach(item => {
-            const li = document.createElement("li");
-            li.innerHTML = `<a href="${item.id}">${item.name}</a>`;
-            menu.appendChild(li);
-        });
-    });
+// fetch("http://localhost:3000")
+//     .then((res) => res.json())
+//     .then((data) => {
+//         const navList = document.getElementById("header");
+//         navList.innerHTML =`
+//             <div>
+//                 <h2>${data.navTitle}</h2>
+//             </div>
+//         `;
+//         data.forEach(item =>{
+            
+//         if(navList){
+//             navList.innerHTML += `
+//                 <nav>
+//                     <ul id="sidemenu">
+//                         <li><a href="${item.navData.navLink}">${item.navData.navName}</a></li>
+//                     </ul>
+//                 </nav>
+//             `;
+//         }
+//         })
+//     });
+
+window.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:3000")
+        .then(res => res.json())
+        .then(data => {
+            const header = document.getElementById("header");
+            if (header){
+                // Insert the nav title
+                header.innerHTML = `
+                    <div>
+                        <h2>${data.navTitle}</h2>
+                    </div>
+                    <nav>
+                        <ul id="sidemenu"></ul>
+                    </nav>
+                `;
+            }
+            // Populate the nav menu items
+            const sidemenu = document.getElementById("sidemenu");
+            data.navData.forEach(item => {
+                // const li = document.createElement("li");
+                // const a = document.createElement("a");
+                // a.href = item.id;
+                // a.textContent = item.name;
+                // li.appendChild(a);
+                // sidemenu.appendChild(li);
+                sidemenu.innerHTML +=`
+                    <li><a href="${item.navLink}">${item.navName}</a></li>
+                `
+            });
+        })
+        .catch(err => console.error("Error loading nav:", err));
+});
