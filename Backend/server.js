cb7668736c0ffbad4277578138fc9a6ac74b01ef
Sahid-Rizwan <sahidrizwan.m@innovationminds.com>
2025-07-04 00:05:04 +0530

    updated the code by calling route function alone here

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,15 @@
 //creating a new server using node.js
 
-const backendData = require('./dataModels/backendData.js');
-const frontendData = require('./dataModels/frontendData.js');
 
 //http module will be used
 const http = require('http'); //importing http package
+const handelRoutes = require('./dataModels/routes')
 const protocol = "http://";
 let host = "localhost";
 let baseUrl = "";
 const server = http.createServer((req,res) => {
     host = req.headers.host;
-    // if (req.url === '/' && req.method === 'GET') {
-        const combinedData = {
-            navTitle: frontendData.navTitle,
-            navData: frontendData.navData
-        };
-        res.writeHead(200, { 
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*' 
-        });
-        res.end(JSON.stringify(combinedData));
-// }
-    // res.writeHead(200,{
-    //     "Content-Type":backendData.contentType.APPJSON,
-    //     "access-control-allow-origin":"*"
-    // });
-    // res.end(JSON.stringify(frontendData.navTitle));
-    // res.end(JSON.stringify(frontendData.navData));
-    
+    handelRoutes(req,res);
 });
 
 const port = 3000;
