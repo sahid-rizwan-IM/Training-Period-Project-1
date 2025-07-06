1ac47ab4aecd35101815714097336b8a2db4a6bf
Sahid-Rizwan <sahidrizwan.m@innovationminds.com>
2025-07-06 23:44:55 +0530

    Created for express

--- Backend/app.js
+++ Backend/app.js
@@ -1,27 +1,24 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+const homePage = require('./express_routes/home_routes');
 const path = require('path'); //to find path based on os by itself.
 // use() - its a middleware function/method - it works for all the routes automatically.
 // get() and post() - these are router methods - works only when a particular url is called or requested.
-app.use(express.json());
-app.use(bodyParser.urlencoded());
+// send() - used in express, it identifies the content-type of the passed data by itself.
+//sendFie() - if a file is passed, it reads the whole file (like html file)
+app.use(express.static(path.join(__dirname, '..', 'Frontend', 'templates')));
+app.use(express.static(path.join(__dirname, '..', 'Frontend', 'static')));
+app.use(express.static(path.join(__dirname, '..', 'Frontend', 'script')));
+app.use(express.static(path.join(__dirname, '..', 'Frontend', 'static', 'images')));
+app.use(homePage);
 
 app.use((req, res, next)=>{
-    console.log("1st Middleware");
-    next();
+    console.log("default");
+    res.status(404).send("Error: Page Not Found!");
 });
-app.use((req, res, next)=>{
-    console.log("2nd Middleware");
-    next()
-});
-app.use((req, res, next)=>{
-    console.log("3rd Middleware");
-    // send() - used in express, it identifies the content-type of the passed data by itself.
-    //sendFie() - if a file is passed, it reads the whole file (like html file)
-    res.send('<h1>This is from epress</h1>');
-    // next();
-});
+
+// app.use(bodyParser.urlencoded());//next handle function
 
 app.listen(3000, ()=>{
     console.log("server running on port 3000");
