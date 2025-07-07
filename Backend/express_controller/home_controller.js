b6d791d02415f505b32db23fed1f3fd131bc14a0
Sahid-Rizwan <sahidrizwan.m@innovationminds.com>
2025-07-07 23:35:27 +0530

    updated the home page like microservice

--- Backend/express_controller/home_controller.js
+++ Backend/express_controller/home_controller.js
@@ -1,4 +1,6 @@
 const frontendData = require('../dataModels/frontendData');
+const path = require('path');
+const rootDir = require('../express_routes/path');
 const homeController = {
     getHomeNavData(req,res){
         try{
@@ -23,20 +25,84 @@ const homeController = {
             })
         }
     },
+
     getHomeContent(req,res){
         try{
-            res.status(200).json(
-                {
+            res.status(200).json({
                     success : true,
                     data : {
                         homeContent : frontendData.homeContent
                     },
                     error : null,
                     status : 200
-                }
-            )
+            })
         }
         catch{
+            res.status(500).json({
+                success : false,
+                data : null,
+                error : err.message,
+                status : 500
+            })
+        }
+    },
+
+    getHomeImages(req, res){
+        try{
+            const imageName = req.query.name;
+            const imagePath = path.join(rootDir, '..', 'Frontend', 'static', 'images', imageName);
+            res.status(200).sendFile(imagePath);
+            //.json({
+            //     success : true,
+            //     data : true,
+            //     error : null,
+            //     status : 200
+            // })
+        }
+        catch(err){
+            res.status(500).json({
+                success : false,
+                data : null,
+                error : err.message,
+                status : 500
+            })
+        }
+    },
+
+    getHomeRoles(req,res){
+        try{
+            res.status(200).json({
+                success : true,
+                data :{
+                    roles : frontendData.roles,
+                    cardsName : frontendData.cardsName
+                },
+                error: null,
+                status : 200
+            })
+        }
+        catch(err){
+            res.status(500).json({
+                success : false,
+                data : null,
+                error : err.message,
+                status : 500
+            })
+        }
+    },
+
+    getHomeFooter(req, res) {
+        try{
+            res.status(200).json({
+                success : true,
+                data : {
+                    footer : frontendData.footer
+                },
+                error : null,
+                status : 200
+            })
+        }
+        catch(err){
             res.status(500).json({
                 success : false,
                 data : null,
--- Backend/express_routes/home_routes.js
+++ Backend/express_routes/home_routes.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const rootDir = require('./path');
-const frontendData = require('../dataModels/frontendData');
 const homeController = require('../express_controller/home_controller');
 
 router.get('/homenavdata', homeController.getHomeNavData);
-
 router.get('/homecontent', homeController.getHomeContent);
-
-router.get('/images', (req, res) => {
-    const imageName = req.query.name;
-    const imagePath = path.join(rootDir, '..', 'Frontend', 'static', 'images', imageName);
-    res.sendFile(imagePath);
-});
-
-router.get('/home-rolescontents',(req,res) => {
-    res.send({
-        roles : frontendData.roles,
-        cardsName : frontendData.cardsName
-    });
-})
+router.get('/images', homeController.getHomeImages);
+router.get('/home-rolescontents',homeController.getHomeRoles);
+router.get('/homefooter', homeController.getHomeFooter);
 
 module.exports = router;
